4096c6d22f3b2bac1201e007d28a7d2ca44880ed
Nithish Kumar Megarajan <nithishkhw05@gmail.com>
2024-01-22 16:59:56 +0530

    mia listens an acts

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import speech_recognition as sr
+import pyttsx3
+import pywhatkit
 
 listener = sr.Recognizer()
-try:
-    with sr.Microphone() as source:
-        print('Listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'mia' in command:
-         print(command)
-except:
-    pass
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'mia' in command:
+                command = command.replace('mia', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_mia():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        print('playing' + song)
+        pywhatkit.playonyt(song)
+
+run_mia()
+
